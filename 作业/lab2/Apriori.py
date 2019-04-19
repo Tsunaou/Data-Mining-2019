7c7fd648685f38787b7ac5332d0a54d7904839a9
Young <895254752@qq.com>
2019-04-19 16:28:01 +0800

    Apriori 重构

--- 作业/lab2/Apriori.py
+++ 作业/lab2/Apriori.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class FileOption:
 
-    dataset_original = []
-    dataset = []
-    D = []
+    dataset_original = []  # 初始读入的数据
+    items = []  # 数据集中项的种类
+    transactions = []   # 数据集
 
     def load_csv(self,filename):
         df = pd.read_csv(filename)  # 这个会直接默认读取到这个Excel的第一个表单
@@ -20,28 +20,26 @@ class FileOption:
             lines = str(lines)
             # print("before:"+lines)
             lines = lines.strip('{}')  # 去除两端的符号
-            # lines = lines.replace('/', ' ')  # 把斜杠转化为空格
+            lines = lines.replace('/', ' ')  # 把斜杠转化为空格
             # print("after :"+lines)
             transaction = lines.split(',')
-            self.D.append(transaction)
+            self.transactions.append(transaction)
             for item in transaction:
                 if not [item] in out:
                     out.append([item])
         out.sort()
-        self.D = list(map(set, self.D))
+        self.transactions = list(map(set, self.transactions))
         # 使用frozenset是为了后面可以将这些值作为字典的键
-        self.dataset = list(map(frozenset, out))  # frozenset一种不可变的集合，set可变集合
+        self.items = list(map(frozenset, out))  # frozenset一种不可变的集合，set可变集合
 
 
     def get_data(self,filename):
         self.get_frozenset(filename=filename)
-        return self.dataset,self.D
+        return self.items, self.transactions
 
 
 
 if __name__ == '__main__':
     fop = FileOption()
-    # dataset,D = fop.get_data('dataset/Groceries_test.csv')
-    # dataset,D = fop.get_data('dataset/test.csv')
-    dataset,D = fop.get_data('dataset/Groceries.csv')
-    print(dataset)
+    items, transactions = fop.get_data('dataset/Groceries.csv')
+    print(items)
